f2d5157dd2eb7bb7c3f92234776f7d87b7025311
DarekZugaj <dariusz.zugaj@gmail.com>
2023-08-23 10:30:16 +0200

    StartMatch made thread-safe

--- ScoreBoard/Controllers/ScoreBoardController.cs
+++ ScoreBoard/Controllers/ScoreBoardController.cs
@@ -8,18 +8,22 @@ namespace ScoreBoard.Controllers
     public class ScoreBoardController : IScoreBoardController
     {
         private ScoreBoard scoreBoard = ScoreBoard.GetScoreBoard();
+        private static readonly object locker = new object();
         public int StartMatch(ITeam homeTeam, ITeam awayTeam)
         {
+            int matchId = 0;
             if (homeTeam == null || string.IsNullOrEmpty(homeTeam.Name))
                 throw new ArgumentException("Home team is incorrect");
             if (awayTeam == null || string.IsNullOrEmpty(awayTeam.Name))
                 throw new ArgumentException("Away team name is incorrect");
 
-            var matchId = scoreBoard.Matches.Count + 1;
-            scoreBoard.Matches.Add(new Match(matchId, homeTeam, 0, awayTeam, 0));
+            lock (locker)
+            {
+                matchId = scoreBoard.Matches.Count + 1;
+                scoreBoard.Matches.Add(new Match(matchId, homeTeam, 0, awayTeam, 0));
+            }
 
             return matchId;
-
         }
     }
 }
