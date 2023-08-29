5e699a94d4198a21564140ef4c1b7edab332ac99
DarekZugaj <dariusz.zugaj@gmail.com>
2023-08-29 15:44:32 +0200

    IValidator committed (was missed in the previous commit)

---
+++ ScoreBoard/Interfaces/IValidator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScoreBoard.Interfaces
+{
+    public interface IValidator
+    {
+        void ValidateStartMatch(ITeam homeTeam, ITeam awayTeam, IEnumerable<IMatch> matches);
+
+        void ValidateUpdateScore(IMatch match, int homeScore, int awayScore);
+
+        void ValidateFinishMatch(IMatch match);
+    }
+}
